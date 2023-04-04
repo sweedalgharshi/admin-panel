454d2ad897bcf91b81660c6197b5ca7daa2b3c96
sweedalgharshi <sweedalgracius@gmail.com>
2023-04-04 08:54:28 +0530

    reuasable chart implementation

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./home.css";
 import FeaturedInfo from "../../components/featured-info/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+
+import { userData } from "../../dummyData";
 
 function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
     </div>
   );
 }
