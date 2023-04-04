1e4a811f0acf14db85b6e34e6e7ee993aa932ad0
sweedalgharshi <sweedalgracius@gmail.com>
2023-04-04 08:17:38 +0530

    created home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/homepage/Home";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="other"> Other Pages</div>
+        <Home />
       </div>
     </div>
   );
